41678d180a0294d8e5e399b714fe21afabc3a74f
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-03 23:35:21 +0800

    [ParkingLot][Koby] feat:SmartParkingBoy Enhancement

--- src/main/java/com/parkinglot/parkingboy/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/parkingboy/SmartParkingBoy.java
@@ -1,15 +1,10 @@
 package com.parkinglot.parkingboy;
 
 import com.parkinglot.Car;
-import com.parkinglot.exception.NoAvailablePositionException;
 import com.parkinglot.ParkingLot;
 import com.parkinglot.Ticket;
-
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-
-import static com.parkinglot.exception.ExceptionMessage.noAvailablePositionExceptionMessage;
 
 public class SmartParkingBoy extends ParkingBoy {
 
@@ -18,10 +13,10 @@ public class SmartParkingBoy extends ParkingBoy {
     }
     @Override
     public Ticket park(Car car){
-        Optional<ParkingLot> validParkingLottoPark = parkingLot.stream().max(Comparator.comparing(ParkingLot::getAvailablePosition));
-        if(!validParkingLottoPark.isPresent())
-            throw new NoAvailablePositionException(noAvailablePositionExceptionMessage);
-        else
-            return validParkingLottoPark.get().park(car);
+        return parkingLot.stream()
+                .max(Comparator.comparing(ParkingLot::getAvailablePosition))
+                .get()
+                .park(car);
+
     }
 }
