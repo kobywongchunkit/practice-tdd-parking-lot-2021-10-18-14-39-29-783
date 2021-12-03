57fa5408716f47c38018961b3ebab30401cca4d7
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-03 23:34:55 +0800

    [ParkingLot][Koby] feat:SuperSmartParkingBoy Enhancement

--- src/main/java/com/parkinglot/parkingboy/SuperSmartParkingBoy.java
+++ src/main/java/com/parkinglot/parkingboy/SuperSmartParkingBoy.java
@@ -1,15 +1,11 @@
 package com.parkinglot.parkingboy;
 
 import com.parkinglot.Car;
-import com.parkinglot.exception.NoAvailablePositionException;
 import com.parkinglot.ParkingLot;
 import com.parkinglot.Ticket;
-
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
-import static com.parkinglot.exception.ExceptionMessage.noAvailablePositionExceptionMessage;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
     public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
