b888b8ffdc7a557735b221077164b821c77e8c6b
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-03 18:52:14 +0800

    [ParkingLot][Koby] test:should_return_ticket_when_park_car_given_smart_parking_boy_with_two_parking_lot_and_parking_lot_1_is_full

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -24,4 +24,21 @@ public class SmartParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailablePosition());
         assertEquals(10,secondParkingLot.getAvailablePosition());
     }
+    @Test
+    void should_return_ticket_when_park_car_given_smart_parking_boy_with_two_parking_lot_and_parking_lot_1_is_full(){
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> ParkingLotList= new ArrayList<>();
+        ParkingLotList.add(firstParkingLot);
+        ParkingLotList.add(secondParkingLot);
+        firstParkingLot.park(new Car());
+        ParkingBoy parkingboy = new SmartParkingBoy(ParkingLotList);
+        //When
+        Ticket ticket = parkingboy.park(new Car());
+        //then
+        assertNotNull(ticket);
+        assertEquals(0, firstParkingLot.getAvailablePosition());
+        assertEquals(9, secondParkingLot.getAvailablePosition());
+    }
 }
