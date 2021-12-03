f3926845527d4621b41c677b41a69986a33666d6
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-03 14:16:30 +0800

    [ParkingLot][Koby] feat:should_return_null_when_park_car_given_full_parking_lot_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -9,8 +9,12 @@ public class ParkingLot {
     public ParkingLot(int capacity){ this.capacity = capacity;}
     public ParkingLot(){this.capacity = 10;}
     public Ticket park(Car car) {
+        if (!hasAvailablePosition()) return null;
         Ticket ticket = new Ticket();
         ticketCarMap.put(ticket, car);
         return ticket;
+    }
+    private boolean hasAvailablePosition(){
+        return ticketCarMap.size() < capacity;
     }
 }
