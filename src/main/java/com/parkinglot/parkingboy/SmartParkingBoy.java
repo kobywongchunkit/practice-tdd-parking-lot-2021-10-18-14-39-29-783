b11c27f4e098e6128b2564c73c69e1f7b8b0f891
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-04 09:29:14 +0800

    [ParkingLot][Koby] feat:SmartParkingBoy error throw enhancement

--- src/main/java/com/parkinglot/parkingboy/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/parkingboy/SmartParkingBoy.java
@@ -3,8 +3,14 @@ package com.parkinglot.parkingboy;
 import com.parkinglot.Car;
 import com.parkinglot.ParkingLot;
 import com.parkinglot.Ticket;
+import com.parkinglot.exception.NoAvailablePositionException;
+import com.parkinglot.exception.UnrecognizedParkingTicketException;
+
 import java.util.Comparator;
 import java.util.List;
+
+import static com.parkinglot.exception.ExceptionMessage.noAvailablePositionExceptionMessage;
+import static com.parkinglot.exception.ExceptionMessage.unrecognizedParkingTicketExceptionMessage;
 
 public class SmartParkingBoy extends ParkingBoy {
 
@@ -15,7 +21,7 @@ public class SmartParkingBoy extends ParkingBoy {
     public Ticket park(Car car){
         return parkingLot.stream()
                 .max(Comparator.comparing(ParkingLot::getAvailablePosition))
-                .get()
+                .orElseThrow(() -> new NoAvailablePositionException(noAvailablePositionExceptionMessage))
                 .park(car);
 
     }
